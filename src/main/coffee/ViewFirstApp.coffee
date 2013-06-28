e4ba17c791a393870c7b76d7c3462eced61629a9
Ricky Barefield <ricky.barefield@googlemail.com>
2013-06-28 22:55:03 +0100

    Begun making a calendar app

--- src/main/coffee/ViewFirstApp.coffee
+++ src/main/coffee/ViewFirstApp.coffee
@@ -2,9 +2,47 @@ require ["ViewFirst", "jquery"], (ViewFirst, $) ->
 
   $ ->
 
-    viewFirst = new ViewFirst("basicView")
+    daysInMonth = (month, year) -> return new Date(year, month, 0).getDate();
+
+    monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+    dayNames = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+    date = new Date(2013, 2, 1)
+
+    viewFirst = new ViewFirst("monthView")
+
+    viewFirst.addSnippet "month", (node) ->
+      node.html(monthNames[date.getMonth() - 1])
+      return node
+
+    viewFirst.addSnippet "calendar", (node) ->
+
+
+      daysInThisMonth = daysInMonth(date.getMonth(), date.getYear())
+
+      template = node.children()
+      template.detach()
+      currentRow = template.clone()
+      node.append(currentRow)
+      currentDayOfWeek = date.getDay()
+      currentDayOfMonth = 1
+
+      while currentDayOfMonth <= daysInThisMonth
+
+        if currentDayOfWeek == 7
+          currentDayOfWeek = 0
+          currentRow = template.clone()
+          node.append(currentRow)
+
+        dayName = dayNames[currentDayOfWeek]
+        currentRow.find(".#{dayName} .date").html("<span>#{currentDayOfMonth}</span>")
+
+        currentDayOfWeek++
+        currentDayOfMonth++
+
+      return node
+
 
     viewFirst.initialize()
 
     #Until routing is working...
-    viewFirst.render("basicView")
+    viewFirst.render("monthView")
--- src/main/webapp/js/ViewFirstApp.js
+++ src/main/webapp/js/ViewFirstApp.js
@@ -2,10 +2,42 @@
 
   require(["ViewFirst", "jquery"], function(ViewFirst, $) {
     return $(function() {
-      var viewFirst;
-      viewFirst = new ViewFirst("basicView");
+      var date, dayNames, daysInMonth, monthNames, viewFirst;
+      daysInMonth = function(month, year) {
+        return new Date(year, month, 0).getDate();
+      };
+      monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      dayNames = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+      date = new Date(2013, 2, 1);
+      viewFirst = new ViewFirst("monthView");
+      viewFirst.addSnippet("month", function(node) {
+        node.html(monthNames[date.getMonth() - 1]);
+        return node;
+      });
+      viewFirst.addSnippet("calendar", function(node) {
+        var currentDayOfMonth, currentDayOfWeek, currentRow, dayName, daysInThisMonth, template;
+        daysInThisMonth = daysInMonth(date.getMonth(), date.getYear());
+        template = node.children();
+        template.detach();
+        currentRow = template.clone();
+        node.append(currentRow);
+        currentDayOfWeek = date.getDay();
+        currentDayOfMonth = 1;
+        while (currentDayOfMonth <= daysInThisMonth) {
+          if (currentDayOfWeek === 7) {
+            currentDayOfWeek = 0;
+            currentRow = template.clone();
+            node.append(currentRow);
+          }
+          dayName = dayNames[currentDayOfWeek];
+          currentRow.find("." + dayName + " .date").html("<span>" + currentDayOfMonth + "</span>");
+          currentDayOfWeek++;
+          currentDayOfMonth++;
+        }
+        return node;
+      });
       viewFirst.initialize();
-      return viewFirst.render("basicView");
+      return viewFirst.render("monthView");
     });
   });
 
